0936b0e693bbf29e0f6e9eb52ff815e8565332df
ndumisomahlangu96 <117864978+ndumisomahlangu96@users.noreply.github.com>
2025-06-13 22:40:43 +0200

    Add files via upload

---
+++ QText_Editor/mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    // This is the most common and recommended way to close a QMainWindow (or any QWidget).
+    this->close();
+}
+
