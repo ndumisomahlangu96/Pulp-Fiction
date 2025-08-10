58b6e7f2865435e65039a483eab996aef61df435
ndumisomahlangu96 <117864978+ndumisomahlangu96@users.noreply.github.com>
2025-08-10 20:49:09 +0200

    Add files via upload

---
+++ QTemplate_Example/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * What
+ * Example
+ *
+ * Why
+ * Templates are independent of any particular type. A template is a blueprint or formula
+ * creating a template class or a function.
+ *
+ * How
+ * See below.
+ */
+
+
+
+#include <QCoreApplication>
+#include <QDebug>                   // Output.
+
+template <typename T>
+void print(T value)
+{
+    qInfo() << value;
+}
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    print<int>(1);
+    print<double>(126.007);
+    print<QString>(QString("Hello"));
+    return a.exec();
+}
